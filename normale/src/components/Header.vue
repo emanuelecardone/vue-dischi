308a1c96a450c36da74778b84e5058a3b87fde8e
emanuelecardone <e_m.cardone@yahoo.it>
2021-12-22 13:44:04 +0100

    Messa la base del layout

---
+++ normale/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header class="w-100 h_100p">
+
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/variables.scss';
+
+    header{
+        background-color: $primary_color;
+    }
+
+</style>
---
+++ normale/src/components/Main.vue
@@ -0,0 +1,22 @@
+<template>
+    <!-- Nel caso continuassi la web-app in futuro devo rimuovere questo vh-100 -->
+    <main class="w-100 vh-100">
+
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'Main'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/variables.scss';
+
+    main{
+        background-color: $tertiary_color;
+    }
+
+
+</style>
