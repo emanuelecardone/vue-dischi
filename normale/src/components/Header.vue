d6dc9a56d6e94471025b4733674e901af080334f
emanuelecardone <e_m.cardone@yahoo.it>
2021-12-22 13:51:58 +0100

    Fatto header

--- normale/src/components/Header.vue
+++ normale/src/components/Header.vue
@@ -1,12 +1,17 @@
 <template>
-    <header class="w-100 h_100p">
-
+    <header class="w-100 h_100p d-flex align-items-center px-4">
+        <img :src="logoSrc" alt="Logo di Spotify" class="w_50p">
     </header>
 </template>
 
 <script>
 export default {
-    name: 'Header'
+    name: 'Header',
+    data: function(){
+        return {
+            logoSrc: require('../assets/img/spotify-logo.png')
+        };
+    }
 }
 </script>
 
