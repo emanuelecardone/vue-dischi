c419e34b6df13c66e146787287503b31ae183179
emanuelecardone <e_m.cardone@yahoo.it>
2021-12-22 15:04:17 +0100

    Fatto ultimo fix (installato roboto condensed)

--- normale/src/components/Disk.vue
+++ normale/src/components/Disk.vue
@@ -2,9 +2,9 @@
     <section class="disk w-100 h-100 d-flex flex-column align-items-center p-4">
         <!-- Verranno letti i valori a cui ho eguagliato diskData (proviene da DisksList) -->
         <img :src="diskData.poster" :alt="'Copertina di ' + diskData.title" class="w_175p h_175p">
-        <h4 class="text-white text-center text-uppercase mt-3 mb-4">{{diskData.title}}</h4>
-        <span class="fs-4 text-center">{{diskData.author}}</span>
-        <span class="fs-4 text-center">{{diskData.year}}</span>
+        <h4 class="text-white text-center text-uppercase mt-4 mb-3">{{diskData.title}}</h4>
+        <span class="fs-5 text-center">{{diskData.author}}</span>
+        <span class="fs-5 text-center">{{diskData.year}}</span>
     </section>
 </template>
 
