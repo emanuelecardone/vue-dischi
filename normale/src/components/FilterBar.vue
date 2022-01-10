6b6ff39fab30edc149055e06a1a11ee94137c14f
emanuelecardone <e_m.cardone@yahoo.it>
2022-01-10 13:34:39 +0100

    Messa la base layout della filter bar

---
+++ normale/src/components/FilterBar.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="filter_wrapper">
+        <select class="genre_select" v-model="genreFilter">
+            <option value="x">test1</option>
+            <option value="y">test2</option>
+            <option value="z">test3</option>
+        </select>
+        <select class="artist_select" v-model="artistFilter">
+            <option value="x">test1</option>
+            <option value="y">test2</option>
+            <option value="z">test3</option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FilterBar',
+    data: function(){
+        return {
+            // Variabile filtro genere
+            genreFilter: null,
+            // Variabile filtro artista
+            artistFilter: null
+        };
+    }    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- normale/src/components/Main.vue
+++ normale/src/components/Main.vue
@@ -1,15 +1,18 @@
 <template>
-    <main class="w-100">
+    <main class="w-100 position-relative">
+        <FilterBar class="position-absolute" />
         <DisksList />
     </main>
 </template>
 
 <script>
+import FilterBar from './FilterBar.vue';
 import DisksList from './DisksList.vue';
 
 export default {
     name: 'Main',
     components: {
+        FilterBar,
         DisksList
     }
 }
@@ -22,5 +25,11 @@ export default {
         background-color: $tertiary_color;
         /* Nel caso continuassi la web-app in futuro devo rimuovere questo vh-100 */
         height: calc(100vh - 100px);
+
+        .filter_wrapper{
+            top: 5%;
+            left: 50%;
+            transform: translateX(-50%);
+        }
     }
 </style>
